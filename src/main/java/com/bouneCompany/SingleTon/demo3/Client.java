4f968e3fbdd4e203b1e5d21d68af407d8c90f89b
bourne-3 <595962708@qq.com>
2021-11-10 21:18:24 +0800

    懒汉式单例

--- src/main/java/com/bouneCompany/SingleTon/demo3/Client.java
+++ src/main/java/com/bouneCompany/SingleTon/demo3/Client.java
@@ -8,6 +8,9 @@ public class Client {
     public static void main(String[] args) {
         SingleTon obj = SingleTon.getObj();
         System.out.println(obj);
+
+        SingleTon obj1 = SingleTon.getObj();
+        System.out.println(obj1);
     }
 
     public void T() {
--- src/main/java/com/bouneCompany/SingleTon/demo3/SingleTon.java
+++ src/main/java/com/bouneCompany/SingleTon/demo3/SingleTon.java
@@ -10,7 +10,10 @@ public class SingleTon {
     static SingleTon obj;
 
     public static SingleTon getObj() {
-        obj = new SingleTon();
+        // 为了防止每次调用getObj的时候都是创建一个新的对象，那这样就不是单例了
+        if (obj == null){
+            obj = new SingleTon();
+        }
         return obj;
     }
 }
